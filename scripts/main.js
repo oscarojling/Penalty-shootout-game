8bbd6e313b30262f258b3fba55c78b7722717058
Oscar Öjling <oscarojling@gmail.com>
2025-11-02 00:34:21 +0100

    team selection and input validation

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,25 @@
 alert("Welcome to my penalty shootout game!")
+
+let playerTeam = prompt("Choose your team: Sweden or Australia")
+if (playerTeam === null) {
+} else if (playerTeam === "Sweden" || playerTeam === "Australia") {
+alert(`You have chosen ${playerTeam}!`)
+}
+
+const DIRECTIONS = ["left", "center", "right"]
+let playerScore = 0;
+let computerScore = 0;
+const TOTAL_ROUNDS = 5;
+
+alert("Now you have to choose at which direction you want shoot!")
+let userInput = prompt("Do you want shoot to the left, center or right?")
+if (userInput === null) {
+
+} 
+while (userInput !== "left" && userInput !== "center" && userInput !== "right") {
+  alert ("Think you misspelled, try again!")
+  userInput = prompt("Do you want shoot to the left, center or right?")
+  if (userInput === null) {
+    
+  }
+} 
