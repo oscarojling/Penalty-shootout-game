a0606d3dbb94b1309e62356ea5122b62b6b35ce6
Oscar Öjling <oscarojling@gmail.com>
2025-11-06 00:02:25 +0100

    Added function to cancel the game

--- scripts/main.js
+++ scripts/main.js
@@ -1,89 +1,35 @@
-alert("Welcome to my penalty shootout game!");
-
-let computerTeam;
-let playerTeam = prompt("Choose your team: Sweden or Australia");
-
-if (playerTeam === null) {
-} else if (playerTeam === "Sweden" || playerTeam === "Australia") {
-  alert(`You have chosen ${playerTeam}!`);
-  if (playerTeam === "Sweden") {
-    computerTeam = "Australia";
-  } else {
-    computerTeam = "Sweden";
+function startPenaltyGame() {
+  alert("Welcome to my penalty shootout game!");
+  let computerTeam;
+  let playerTeam = prompt("Choose your team: Sweden or Australia");
+  if (playerTeam === null) {
+    alert("You have cancelled the game");
+    return;
   }
-} else {
-  alert("You have not chosen a valid team!");
-}
-
-const DIRECTIONS = ["left", "center", "right"];
-let playerScore = 0;
-let computerScore = 0;
-const TOTAL_ROUNDS = 5;
-
-for (let round = 1; round <= TOTAL_ROUNDS; round++) {
-  alert(`Round: ${round} - Choose where to shoot!`);
-  let userInput = prompt("Do you want shoot to the left, center or right?");
-  if (userInput === null) {
-  }
-  while (
-    userInput !== "left" &&
-    userInput !== "center" &&
-    userInput !== "right"
-  ) {
-    alert("Think you misspelled, try again!");
-    userInput = prompt("Do you want shoot to the left, center or right?");
-    if (userInput === null) {
+  playerTeam = playerTeam.toLowerCase();
+  if (playerTeam === "sweden" || playerTeam === "australia") {
+    alert(`You have chosen ${playerTeam}!`);
+    if (playerTeam === "sweden") {
+      computerTeam = "australia";
+    } else {
+      computerTeam = "sweden";
     }
-  }
-  playerShotDirection = userInput;
-  let computerKeeperDirection =
-    DIRECTIONS[Math.floor(Math.random() * DIRECTIONS.length)];
-  if (playerShotDirection !== computerKeeperDirection) {
-    alert(`GOOOOOAAAL!`);
-    playerScore++;
   } else {
-    alert("What a save by the keeper!");
+    alert("You have not chosen a valid team!");
+    return;
   }
-  alert(
-    `This is how the scores stand! ${playerTeam} ${playerScore} - ${computerTeam} ${computerScore}`
-  );
-  alert("Now the computer is shooting!");
-  let diveInput = prompt("Where do you want to dive? left, center or right?");
-  if (diveInput === null) {
-  }
-  while (
-    diveInput !== "left" &&
-    diveInput !== "center" &&
-    diveInput !== "right"
-  ) {
-    alert("Think you misspelled, try again!");
-    diveInput = prompt("Where do you want to dive? left, center or right?");
-    if (diveInput === null) {
-    }
-  }
-  let computerShotDirection =
-    DIRECTIONS[Math.floor(Math.random() * DIRECTIONS.length)];
-  if (computerShotDirection !== diveInput) {
-    alert(`GOOOOOOAAAL!`);
-    computerScore++;
-  } else {
-    alert("What a save! Stunning footwork!");
-  }
-  alert(
-    `This is how the scores stand! ${playerTeam} ${playerScore} - ${computerTeam} ${computerScore}`
-  );
-}
-if (playerScore > computerScore) {
-  alert(`${playerTeam} wins!`);
-} else if (computerScore > playerScore) {
-  alert(`${computerTeam} wins!`);
-} else {
-  alert("We are going to sudden death!");
-  while (playerScore === computerScore) {
-    alert("Time for sudden death! Good luck!");
+  const DIRECTIONS = ["left", "center", "right"];
+  let playerScore = 0;
+  let computerScore = 0;
+  const TOTAL_ROUNDS = 5;
+  for (let round = 1; round <= TOTAL_ROUNDS; round++) {
+    alert(`Round: ${round} - Choose where to shoot!`);
     let userInput = prompt("Do you want shoot to the left, center or right?");
     if (userInput === null) {
+      alert("You have cancelled the game");
+      return;
     }
+    userInput = userInput.toLowerCase();
     while (
       userInput !== "left" &&
       userInput !== "center" &&
@@ -92,7 +38,10 @@ if (playerScore > computerScore) {
       alert("Think you misspelled, try again!");
       userInput = prompt("Do you want shoot to the left, center or right?");
       if (userInput === null) {
+        alert("You have cancelled the game");
+        return;
       }
+      userInput = userInput.toLowerCase();
     }
     playerShotDirection = userInput;
     let computerKeeperDirection =
@@ -106,10 +55,13 @@ if (playerScore > computerScore) {
     alert(
       `This is how the scores stand! ${playerTeam} ${playerScore} - ${computerTeam} ${computerScore}`
     );
-    alert("Now the computer is shooting!");
+    alert(`Now it's time for ${computerTeam} to take the penalty`);
     let diveInput = prompt("Where do you want to dive? left, center or right?");
     if (diveInput === null) {
+      alert("You have cancelled the game");
+      return;
     }
+    diveInput = diveInput.toLowerCase();
     while (
       diveInput !== "left" &&
       diveInput !== "center" &&
@@ -118,8 +70,11 @@ if (playerScore > computerScore) {
       alert("Think you misspelled, try again!");
       diveInput = prompt("Where do you want to dive? left, center or right?");
       if (diveInput === null) {
+        alert("You have cancelled the game");
+        return;
       }
     }
+    diveInput = diveInput.toLowerCase();
     let computerShotDirection =
       DIRECTIONS[Math.floor(Math.random() * DIRECTIONS.length)];
     if (computerShotDirection !== diveInput) {
@@ -131,13 +86,88 @@ if (playerScore > computerScore) {
     alert(
       `This is how the scores stand! ${playerTeam} ${playerScore} - ${computerTeam} ${computerScore}`
     );
-    if (playerScore !== computerScore) {
-      break;
-    } 
-    if (playerScore > computerScore) {
-      alert(`${playerTeam} wins!`)
-    } else {
-      alert (`${computerTeam} wins!`)
+  }
+  if (playerScore > computerScore) {
+    alert(`${playerTeam} wins!`);
+  } else if (computerScore > playerScore) {
+    alert(`${computerTeam} wins!`);
+  } else {
+    alert("We are going to sudden death!");
+    while (playerScore === computerScore) {
+      alert("Time for sudden death! Good luck!");
+      let userInput = prompt("Do you want shoot to the left, center or right?");
+      if (userInput === null) {
+        alert("You have cancelled the game");
+        return;
+      }
+      userInput = userInput.toLowerCase();
+      while (
+        userInput !== "left" &&
+        userInput !== "center" &&
+        userInput !== "right"
+      ) {
+        alert("Think you misspelled, try again!");
+        userInput = prompt("Do you want shoot to the left, center or right?");
+        if (userInput === null) {
+          alert("You have cancelled the game");
+          return;
+        }
+      }
+      userInput = userInput.toLowerCase();
+      playerShotDirection = userInput;
+      let computerKeeperDirection =
+        DIRECTIONS[Math.floor(Math.random() * DIRECTIONS.length)];
+      if (playerShotDirection !== computerKeeperDirection) {
+        alert(`GOOOOOAAAL! for ${playerTeam}`);
+        playerScore++;
+      } else {
+        alert("What a save by the keeper!");
+      }
+      alert(
+        `This is how the scores stand! ${playerTeam} ${playerScore} - ${computerTeam} ${computerScore}`
+      );
+      alert(`Now it's time for ${computerTeam} to take the penalty`);
+      let diveInput = prompt(
+        "Where do you want to dive? left, center or right?"
+      );
+      if (diveInput === null) {
+        alert("You have cancelled the game");
+        return;
+      }
+      diveInput = diveInput.toLowerCase();
+      while (
+        diveInput !== "left" &&
+        diveInput !== "center" &&
+        diveInput !== "right"
+      ) {
+        alert("Think you misspelled, try again!");
+        diveInput = prompt("Where do you want to dive? left, center or right?");
+        if (diveInput === null) {
+          alert("You have cancelled the game");
+          return;
+        }
+        diveInput = diveInput.toLowerCase();
+      }
+      let computerShotDirection =
+        DIRECTIONS[Math.floor(Math.random() * DIRECTIONS.length)];
+      if (computerShotDirection !== diveInput) {
+        alert(`GOOOOOOAAAL! for ${computerTeam}`);
+        computerScore++;
+      } else {
+        alert("What a save! Stunning footwork!");
+      }
+      alert(
+        `This is how the scores stand! ${playerTeam} ${playerScore} - ${computerTeam} ${computerScore}`
+      );
+      if (playerScore !== computerScore) {
+        return;
+      }
+      if (playerScore > computerScore) {
+        alert(`${playerTeam} wins!`);
+      } else {
+        alert(`${computerTeam} wins!`);
+      }
     }
   }
 }
+startPenaltyGame();
